e0f9e7884c76af4cb3412b36660ce9601b99bacf
Martin Mostad <martimos@ntnu.no>
2018-10-08 01:11:06 +0200

    pushing so ambj can have a look at instability issue

--- InitialisePart1.m
+++ InitialisePart1.m
@@ -1,26 +1,27 @@
-dag2rad = pi/180;
-radg2dag = 180/pi;
+deg2rad = pi/180;
+rad2deg = 180/pi;
 
-g = -9.81;
+g = 9.81;
 V_a = 580*1000/3600;
 V_g = V_a;
-d = 1.5*dag2rad;
+d = 1.5*deg2rad;
 
 a_phi_1 = 2.87;
 a_phi_2 = -0.65;
 
-delta_a_max = 30*dag2rad;
-e_max = 15*dag2rad;
+delta_a_max = 30*deg2rad;
+e_max = 15*deg2rad;
 omega_n = sqrt(abs(a_phi_2)*delta_a_max/e_max);
 zeta = 0.707;
-
+omega_x = 1/5 * omega_n;
+zeta_x = 5;
 
 k_d_phi = (2*zeta*omega_n-a_phi_1)/a_phi_2;
 k_p_phi = delta_a_max/e_max*sign(a_phi_2);
-k_i_phi = 0;
+k_i_phi = -1;
 
-k_p_khi = 0;
-k_i_khi = 0;
+k_p_khi = (V_g/g)*2*zeta_x*omega_x;
+k_i_khi = (V_g/g)*(omega_x)^2;
 
 Desiered_khi = 0;
 
