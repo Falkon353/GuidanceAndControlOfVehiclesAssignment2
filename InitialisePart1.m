622ff29520bad11661a773b4b2fa346264c96678
Martin Mostad <martimos@ntnu.no>
2018-10-07 17:11:01 +0200

    initial commit

---
+++ InitialisePart1.m
@@ -0,0 +1,19 @@
+dag2reg = pi/180;
+reg2dag = 180/pi;
+
+g = -9.81;
+V_a = 580*1000/3600;
+V_g = V_a;
+d = 1.5*dag2reg;
+
+a_phi_1 = 2.87;
+a_phi_2 = -0.05;
+
+k_d_phi = 0;
+k_p_phi = 0;
+k_i_phi = 0;
+
+k_p_khi = 0;
+k_i_khi = 0;
+
+Desiered_khi = 0;
